a42a23ea3c830291bc0fafa0d578f326eaec5e12
ricardodragon <ricardoelfuego@gmail.com>
2023-02-16 16:58:22 -0300

    teste

--- src/estrutura/LabelInput/index.js
+++ src/estrutura/LabelInput/index.js
@@ -15,7 +15,7 @@ LabelInput(props){
     return (
         <>
             <label style={{whiteSpace:"nowrap"}} className="p-1" htmlFor={props.id}>{props.label}</label>            
-            <input autoComplete="given" readOnly={props.readonly} placeholder={props.placeholder} list={props.list} onChange={onChange} value={props.value} id={props.id} disabled={props.disabled} required={props.required} step={props.step} size={props.size} type={props.type} className={"form-control form-control-sm"}/>                        
+            <input autoComplete="true" readOnly={props.readonly} placeholder={props.placeholder} list={props.list} onChange={onChange} value={props.value} id={props.id} disabled={props.disabled} required={props.required} step={props.step} size={props.size} type={props.type} className={"form-control form-control-sm"}/>                        
         </>        
     )
 }
