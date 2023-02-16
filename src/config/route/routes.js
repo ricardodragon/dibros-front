7e35b41b551f601ff5ec6f849fb9dfcd7a6deb1a
ricardodragon <ricardoelfuego@gmail.com>
2023-02-16 05:37:32 -0300

    teste

--- src/config/route/routes.js
+++ src/config/route/routes.js
@@ -5,7 +5,7 @@ import PrivateRoutes from "./private-routes";
 import Usuarios from "../../pages/Usuarios"
 import Contas from "../../pages/Contas"
 import Produtos from "../../pages/Produtos";
-import Detalhes from "../../pages/Anuncios/Detalhes";
+// import Detalhes from "../../pages/Anuncios/Detalhes";
 import Listagem from "../../pages/Anuncios/Listagem";
 
 const Routes = ()=>(
@@ -15,7 +15,7 @@ const Routes = ()=>(
             <PrivateRoutes exact path={["/","/home"]} component={Home} nome={"Seja bem vindo a FlexConnection!"}/>
             <PrivateRoutes exact path="/usuarios" nome="Indentificação usuário" component={Usuarios}/>
             <PrivateRoutes exact path="/contas" nome={"Gerencie suas contas MercadoLivre"} component={Contas}/>                       
-            <PrivateRoutes exact path="/anuncios/detalhes/:idAnuncio/:userId" nome={"Anúncios - detalhes"} component={Detalhes}/>            
+            {/* <PrivateRoutes exact path="/anuncios/detalhes/:idAnuncio/:userId" nome={"Anúncios - detalhes"} component={Detalhes}/>             */}
             <PrivateRoutes exact path="/anuncios/:id/:sku" nome={"Anúncios"} component={Listagem}/>            
             <PrivateRoutes exact path="/produtos" nome={"Produtos"} component={Produtos}/>
             <Redirect to="/"/>
