6e671a78a8cbad37c09508a5c721f496349fa9d7
ricardodragon <ricardoelfuego@gmail.com>
2023-02-16 21:09:35 -0300

    teste

--- src/config/route/routes.js
+++ src/config/route/routes.js
@@ -12,10 +12,10 @@ const Routes = ()=>(
     <BrowserRouter>
         <Switch>
             <Route exact path="/login" component={Login}></Route>
-            <PrivateRoutes exact path={["/","/home"]} component={Home} nome={"Seja bem vindo a FlexConnection!"}/>
+            <PrivateRoutes exact path={["/","/home"]} component={Home} nome={"Seja bem vindo a dibros-store!"}/>
             <PrivateRoutes exact path="/usuarios" nome="Indentificação usuário" component={Usuarios}/>
             <PrivateRoutes exact path="/contas" nome={"Gerencie suas contas MercadoLivre"} component={Contas}/>                       
-            <PrivateRoutes exact path="/anuncios/detalhes/:idAnuncio/:userId" nome={"Anúncios - detalhes"} component={Detalhes}/>            
+            {/* <PrivateRoutes exact path="/anuncios/detalhes/:idAnuncio/:userId" nome={"Anúncios - detalhes"} component={Detalhes}/>             */}
             <PrivateRoutes exact path="/anuncios/:id/:sku" nome={"Anúncios"} component={Listagem}/>            
             <PrivateRoutes exact path="/produtos" nome={"Produtos"} component={Produtos}/>
             <Redirect to="/"/>
