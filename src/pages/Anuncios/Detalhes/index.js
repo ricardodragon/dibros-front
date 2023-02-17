5b31c5d6b8bd4234bac8e9cf3c18680d6a874ec9
ricardodragon <ricardoelfuego@gmail.com>
2023-02-17 16:33:10 -0300

    teste

--- src/pages/Anuncios/Detalhes/index.js
+++ src/pages/Anuncios/Detalhes/index.js
@@ -3,9 +3,11 @@ import "./detalhes.css"
 function Detalhes(){
     
     return (   
-        <>           
-            <h1>Oi</h1>                                                                                                                                                                                     
-        </>        
+        <div style={{ position: "absolute", width:"100%", height:"100%", backgroundColor:"white", zIndex:"1000" }}>
+            <div className="spinner-border p-5" style={{width: "3rem",height: "3rem", margin:"10% 0 0 30%"}} role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>                 
+        </div>                                                                                                                                                                                   
     )
 }
 export default Detalhes
