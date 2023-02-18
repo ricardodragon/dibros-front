8c6b73f278530babd4764398d082062db8287e63
ricardodragon <ricardoelfuego@gmail.com>
2023-02-18 09:14:08 -0300

    teste

--- src/pages/Anuncios/Detalhes/index.js
+++ src/pages/Anuncios/Detalhes/index.js
@@ -57,7 +57,7 @@ function Detalhes(){
         <>
             <div className={"alert alert-success "+(values.ok?"":"visually-hidden")} role="alert"><FcCheckmark/> Anuncio enviado com sucesso</div>
             <div className={"alert alert-danger "+(values.erro?"":"visually-hidden")} role="alert"><FcHighPriority/>Erro: {values.erro}</div>
-            {/* <LabelInput readonly={true} label="Visitas" disabled={true} value={0}/> */}
+            <LabelInput readonly={true} label="Visitas" disabled={true} value={values.visits}/>
             <button className="btn btn-sm btn-success" onClick={event=>{event.preventDefault();setVisits()}}></button>                                                                                                                                                    
             <form onSubmit={event => {event.preventDefault();}}> 
                 <div className="d-flex justify-content-end">
