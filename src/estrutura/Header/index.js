12d79be8e6c9411c700432d5baf87bd45ee21903
ricardo <ricardoelfuego@gmail.com>
2026-04-08 04:32:00 -0300

    teste

--- src/estrutura/Header/index.js
+++ src/estrutura/Header/index.js
@@ -12,8 +12,7 @@ function Header(){
     const [values, setValues] = useState({})
     const [notificacoesQTD, setNotificacoesQTD] = useState(0);
     const host = process.env.REACT_APP_URL;
-    const notificationIcon = "m13.497 4.898.053.8.694.4C15.596 6.878 16.5 8.334 16.5 10v2.892c0 .997.27 1.975.784 2.83L18.35 17.5H5.649l1.067-1.778c.513-.855.784-1.833.784-2.83V10c0-1.666.904-3.122 2.256-3.902l.694-.4.053-.8c.052-.78.703-1.398 1.497-1.398.794 0 1.445.618 1.497 1.398ZM6 10c0-2.224 1.21-4.165 3.007-5.201C9.11 3.236 10.41 2 12 2c1.59 0 2.89 1.236 2.993 2.799C16.79 5.835 18 7.776 18 10v2.892c0 .725.197 1.436.57 2.058l1.521 2.535c.4.667-.08 1.515-.857 1.515H15c0 .796-.316 1.559-.879 2.121-.562.563-1.325.879-2.121.879s-1.559-.316-2.121-.879C9.316 20.56 9 19.796 9 19H4.766c-.777 0-1.257-.848-.857-1.515L5.43 14.95c.373-.622.57-1.333.57-2.058V10Zm4.5 9c0 .398.158.78.44 1.06.28.282.662.44 1.06.44s.78-.158 1.06-.44c.282-.28.44-.662.44-1.06h-3Z";
-    
+
     useEffect(() => 
         localStorage.getItem('token')?
             axios.get("/auth/usuarios").then(response=> {
@@ -64,15 +63,20 @@ function Header(){
                 </ul>
             </nav> */}
             {values.usuario&&
-                <><label className="notificacao-botao" htmlFor="notificacao-check">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" focusable="false" aria-hidden="true" style={{borderRadius:'50%', backgroundColor:'#858080'}}>
-                        <path d={notificationIcon}></path>
-                    </svg>      
-                    {(values.notificacaoQtd>0||notificacoesQTD>0)&&<div>{values.notificacaoQtd+notificacoesQTD}</div>}
-                </label>
+                <>
                 
+                <label className="notificacao-botao" htmlFor="notificacao-check">
+                    🔔
+                    {(values.notificacaoQtd>0||notificacoesQTD>0)&&<div className='notificacao-qtd'>{values.notificacaoQtd+notificacoesQTD}</div>}                                    
+                </label>
                 <input type="checkbox" className="notificacao-check" id="notificacao-check" onChange={getNotificacoes}/>
                 
+                <label className="carrinho-botao" htmlFor="carrinho-check">
+                    🛒
+                    {localStorage.getItem('carrinho')&&<div className='carrinho-qtd'>{JSON.parse(localStorage.getItem('carrinho')).length}</div>}                                    
+                </label>
+                <input type="checkbox" className="carrinho-check" id="carrinho-check"/>
+
                 <div id="notificacao-menu">
                     {values.notificacoes&&values.notificacoes.map((n, index)=>{
                         if(n.tipo==='SEGUIDO') return <Seguidor key={index} index={index} setNotificacao={setNotificacao} removeNotificacao={removeNotificacao} id={n.id}/>;
--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -4,7 +4,9 @@
 function Carrinho(props){
 
     return (
-        <></>
+        <>
+        
+        </>
     );
 }
 
